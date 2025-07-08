4630183dbb9df2a777208bcc71caad0fa9212203
Atish <125atishprak@gmail.com>
2025-07-08 22:18:02 +0530

    hover effect

--- src/Movies/MovieCard.jsx
+++ src/Movies/MovieCard.jsx
@@ -1,8 +1,8 @@
 
 export function MovieCard({movie}) {
   return (
-        <div className="flex flex-col gap-2 justify-between cursor-pointer w-54 rounded-2xl">
-            <div className="h-80 w-54 object-contain ">
+        <div className="flex flex-col gap-2 group justify-between cursor-pointer w-54 rounded-2xl">
+            <div className="h-80 w-54 object-contain hover:border-2 hover:border-white hover:rounded-2xl ">
                 <img src={movie.poster_path} className="rounded-2xl"/>
             </div>
             <span className="text-white overflow-hidden whitespace-nowrap">{movie.original_title}</span>
